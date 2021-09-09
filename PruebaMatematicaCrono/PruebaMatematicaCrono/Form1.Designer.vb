394b099e67d0377201ba6dabd150b058d623d9f6
martinagustin <82984398+martinagustin@users.noreply.github.com>
2021-09-09 10:20:52 -0300

    Add files via upload

---
+++ PruebaMatematicaCrono/PruebaMatematicaCrono/Form1.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.timeLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.plusLeftLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.plusRightLabel = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.suma = New System.Windows.Forms.NumericUpDown()
+        Me.resta = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.minusRightLabel = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.minusLeftLabel = New System.Windows.Forms.Label()
+        Me.producto = New System.Windows.Forms.NumericUpDown()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.timesRightLabel = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.timesLeftLabel = New System.Windows.Forms.Label()
+        Me.coeficiente = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.dividedRightLabel = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.dividedLeftLabel = New System.Windows.Forms.Label()
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.suma, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.resta, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.producto, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.coeficiente, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'timeLabel
+        '
+        Me.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.timeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.timeLabel.Location = New System.Drawing.Point(272, 9)
+        Me.timeLabel.Name = "timeLabel"
+        Me.timeLabel.Size = New System.Drawing.Size(200, 25)
+        Me.timeLabel.TabIndex = 0
+        Me.timeLabel.Text = "Label1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(54, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(167, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tiempo restante"
+        '
+        'plusLeftLabel
+        '
+        Me.plusLeftLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.plusLeftLabel.Location = New System.Drawing.Point(29, 75)
+        Me.plusLeftLabel.Name = "plusLeftLabel"
+        Me.plusLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.plusLeftLabel.TabIndex = 2
+        Me.plusLeftLabel.Text = "?"
+        Me.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(86, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 50)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "+"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'plusRightLabel
+        '
+        Me.plusRightLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.plusRightLabel.Location = New System.Drawing.Point(152, 75)
+        Me.plusRightLabel.Name = "plusRightLabel"
+        Me.plusRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.plusRightLabel.TabIndex = 4
+        Me.plusRightLabel.Text = "?"
+        Me.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(218, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 50)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "="
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'suma
+        '
+        Me.suma.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.suma.Location = New System.Drawing.Point(284, 82)
+        Me.suma.Name = "suma"
+        Me.suma.Size = New System.Drawing.Size(100, 39)
+        Me.suma.TabIndex = 2
+        '
+        'resta
+        '
+        Me.resta.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.resta.Location = New System.Drawing.Point(284, 132)
+        Me.resta.Name = "resta"
+        Me.resta.Size = New System.Drawing.Size(100, 39)
+        Me.resta.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(218, 125)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 50)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "="
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'minusRightLabel
+        '
+        Me.minusRightLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.minusRightLabel.Location = New System.Drawing.Point(152, 125)
+        Me.minusRightLabel.Name = "minusRightLabel"
+        Me.minusRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.minusRightLabel.TabIndex = 9
+        Me.minusRightLabel.Text = "?"
+        Me.minusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(86, 125)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(60, 50)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "-"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'minusLeftLabel
+        '
+        Me.minusLeftLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.minusLeftLabel.Location = New System.Drawing.Point(29, 125)
+        Me.minusLeftLabel.Name = "minusLeftLabel"
+        Me.minusLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.minusLeftLabel.TabIndex = 7
+        Me.minusLeftLabel.Text = "?"
+        Me.minusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'producto
+        '
+        Me.producto.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.producto.Location = New System.Drawing.Point(284, 182)
+        Me.producto.Name = "producto"
+        Me.producto.Size = New System.Drawing.Size(100, 39)
+        Me.producto.TabIndex = 4
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.Location = New System.Drawing.Point(218, 175)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(60, 50)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "="
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'timesRightLabel
+        '
+        Me.timesRightLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.timesRightLabel.Location = New System.Drawing.Point(152, 175)
+        Me.timesRightLabel.Name = "timesRightLabel"
+        Me.timesRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.timesRightLabel.TabIndex = 14
+        Me.timesRightLabel.Text = "?"
+        Me.timesRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(86, 175)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(60, 50)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "x"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'timesLeftLabel
+        '
+        Me.timesLeftLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.timesLeftLabel.Location = New System.Drawing.Point(29, 175)
+        Me.timesLeftLabel.Name = "timesLeftLabel"
+        Me.timesLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.timesLeftLabel.TabIndex = 12
+        Me.timesLeftLabel.Text = "?"
+        Me.timesLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'coeficiente
+        '
+        Me.coeficiente.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.coeficiente.Location = New System.Drawing.Point(284, 232)
+        Me.coeficiente.Name = "coeficiente"
+        Me.coeficiente.Size = New System.Drawing.Size(100, 39)
+        Me.coeficiente.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(218, 225)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 50)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "="
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dividedRightLabel
+        '
+        Me.dividedRightLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.dividedRightLabel.Location = New System.Drawing.Point(152, 225)
+        Me.dividedRightLabel.Name = "dividedRightLabel"
+        Me.dividedRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.dividedRightLabel.TabIndex = 19
+        Me.dividedRightLabel.Text = "?"
+        Me.dividedRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(86, 225)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(60, 50)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "÷ "
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dividedLeftLabel
+        '
+        Me.dividedLeftLabel.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.dividedLeftLabel.Location = New System.Drawing.Point(29, 225)
+        Me.dividedLeftLabel.Name = "dividedLeftLabel"
+        Me.dividedLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.dividedLeftLabel.TabIndex = 17
+        Me.dividedLeftLabel.Text = "?"
+        Me.dividedLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'startButton
+        '
+        Me.startButton.AutoSize = True
+        Me.startButton.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.startButton.Location = New System.Drawing.Point(152, 310)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(159, 35)
+        Me.startButton.TabIndex = 1
+        Me.startButton.Text = "Iniciar la prueba"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(480, 357)
+        Me.Controls.Add(Me.startButton)
+        Me.Controls.Add(Me.coeficiente)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.dividedRightLabel)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.dividedLeftLabel)
+        Me.Controls.Add(Me.producto)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.timesRightLabel)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.timesLeftLabel)
+        Me.Controls.Add(Me.resta)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.minusRightLabel)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.minusLeftLabel)
+        Me.Controls.Add(Me.suma)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.plusRightLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.plusLeftLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.timeLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.suma, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.resta, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.producto, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.coeficiente, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents timeLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents plusLeftLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents plusRightLabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents suma As NumericUpDown
+    Friend WithEvents resta As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents minusRightLabel As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents minusLeftLabel As Label
+    Friend WithEvents producto As NumericUpDown
+    Friend WithEvents Label8 As Label
+    Friend WithEvents timesRightLabel As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents timesLeftLabel As Label
+    Friend WithEvents coeficiente As NumericUpDown
+    Friend WithEvents Label5 As Label
+    Friend WithEvents dividedRightLabel As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents dividedLeftLabel As Label
+    Friend WithEvents startButton As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ PruebaMatematicaCrono/PruebaMatematicaCrono/Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+    'denominamos variables con su respectivo tipo
+    'creamos variable randomizer que pertenece al objeto Random
+    Private randomizer As New Random
+    Private addend1 As Integer
+    Private addend2 As Integer
+    Private minuend As Integer
+    Private subtrahend As Integer
+    Private multiplicand As Integer
+    Private multiplier As Integer
+    Private dividend As Integer
+    Private divisor As Integer
+    Private timeLeft As Integer
+
+
+
+    Public Sub StartTheQuizz()
+        ' Generate two random numbers to add.GENERA DOS NUMEROS ALEATORIOS PARA LA SUMA
+        ' Store the values in the variables 'addend1' and 'addend2'.ALMACENA LOS VALORES EN LAS VARIABLES. ADDEND
+
+        addend1 = randomizer.Next(51)
+        addend2 = randomizer.Next(51)
+        'la funcion next(n) de randomizer da numeros al azar desde 0 hasta n-1
+        ' Convert the two randomly generated numbers|convierte los dos numero aleatorios en cadenas para que se puedan mostrar
+        ' into strings so that they can be displayed
+        ' in the label controls.
+        plusLeftLabel.Text = addend1.ToString()
+        plusRightLabel.Text = addend2.ToString()
+        'relacionamos las variables addend con su respectivo label, ademas las convertimos en string para mostrarlas.
+
+        ' 'sum' is the name of the NumericUpDown control.
+        ' This step makes sure its value is zero before|suma es el name del primero numeric, esto aseguraque se inicie en valor 0
+        ' adding any values to it.
+        suma.Value = 0
+        minuend = randomizer.Next(1, 101)
+        subtrahend = randomizer.Next(1, minuend)
+        minusLeftLabel.Text = minuend.ToString()
+        minusRightLabel.Text = subtrahend.ToString()
+        resta.Value = 0
+
+        ' Fill in the multiplication problem.
+        multiplicand = randomizer.Next(2, 11)
+        multiplier = randomizer.Next(2, 11)
+        timesLeftLabel.Text = multiplicand.ToString()
+        timesRightLabel.Text = multiplier.ToString()
+        producto.Value = 0
+
+        ' Fill in the division problem.
+        divisor = randomizer.Next(2, 11)
+        Dim temporaryQuotient As Integer = randomizer.Next(2, 11)
+        dividend = divisor * temporaryQuotient
+        dividedLeftLabel.Text = dividend.ToString()
+        dividedRightLabel.Text = divisor.ToString()
+        coeficiente.Value = 0
+        'iniciar el timer
+        timeLeft = 30
+        timeLabel.Text = "30 seconds"
+        Timer1.Start()
+
+    End Sub
+    Public Function CheckTheAnswer() As Boolean
+
+        If addend1 + addend2 = suma.Value AndAlso
+            minuend - subtrahend = resta.Value AndAlso
+            multiplicand * multiplier = producto.Value AndAlso
+            dividend / divisor = coeficiente.Value Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub startButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
+        'se llama al metodo startthequiz
+        StartTheQuizz()
+        startButton.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If CheckTheAnswer() Then
+            Timer1.Stop()
+            MessageBox.Show("You got all of the answers right!", "Felicitaciones!")
+            startButton.Enabled = True
+        ElseIf timeLeft > 0 Then
+            'si tiempo restante> 0 entonces
+            ' Display the new time left\mostrar nuevo timepo restante
+            ' by updating the Time Left label.\actualizar tiempo.
+            timeLeft -= 1
+            timeLabel.Text = timeLeft & " seconds"
+        Else
+            ' If the user ran out of time, stop the timer, show
+            ' a MessageBox, and fill in the answers.
+            'si se ha quedado sin tiempo, para el timer y muestra un mensaje y completa las respuestas.
+            Timer1.Stop()
+            timeLabel.Text = "Time's up!"
+            MessageBox.Show("You didn't finish in time.| te has quedado sin tiempo", "Sorry!")
+            suma.Value = addend1 + addend2
+            resta.Value = minuend - subtrahend
+            producto.Value = multiplicand * multiplier
+            coeficiente.Value = dividend / divisor
+            startButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub answer_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles suma.Enter, resta.Enter, producto.Enter, coeficiente.Enter
+
+        ' Select the whole answer in the NumericUpDown control.
+        Dim answerBox = TryCast(sender, NumericUpDown)
+
+        If answerBox IsNot Nothing Then
+            Dim lengthOfAnswer = answerBox.Value.ToString().Length
+            answerBox.Select(0, lengthOfAnswer)
+        End If
+
+    End Sub
+End Class
